f09051e57818c0617e7070ba306cf909554ba99d
TerryHC <112371712+cdexswzaq0110@users.noreply.github.com>
2025-02-25 14:50:01 +0800

    Update 0221-1.cpp

--- Basic Programming Problems/0221-1.cpp
+++ Basic Programming Problems/0221-1.cpp
@@ -1,8 +1,8 @@
-#include<iostream>//1.對齊右邊 
+#include<iostream>//1.
 using namespace std;
 int main(){
 	int height;
-	cout<<"請輸入三角形高度=> ";
+	cout<<"Please enter the triangle height=> ";
     cin>>height;
     cout<<endl;
 	for(int i=1;i<=height;i++){
