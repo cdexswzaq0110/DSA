51ad9ea6ec8e09262739b37eabe13b201887f75d
TerryHC <112371712+cdexswzaq0110@users.noreply.github.com>
2025-02-25 14:56:32 +0800

    Update triangle.cpp

--- Basic Programming Problems/triangle.cpp
+++ Basic Programming Problems/triangle.cpp
@@ -1,25 +1,2 @@
-#include<iostream>//3.兩個左右對齊 
-using namespace std;
-int main(){
-	int h;
-	cout<<"請輸入三角形高度=> ";
-    cin>>h;
-    cout<<endl;
-	for(int i=1;i<=h;i++){
-		for(int j=1;j<=h-i;j++){
-			cout<<' ';
-		}
-		for(int j=1;j<=2*i-1;j++){
-			cout<<'*';
-		}
-		for(int j=1;j<=h-i;j++){
-			cout<<' ';
-			cout<<' ';
-		}
-		for(int j=1;j<=2*i-1;j++){
-			cout<<'*';
-		}
-		cout<<endl;
-	} 
-	return 0;
-}
+//0221-triangle practice *
+
