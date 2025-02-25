26511cb67b799e627266ebf0e2093b7a3799adea
TerryHC <112371712+cdexswzaq0110@users.noreply.github.com>
2025-02-25 15:06:00 +0800

    Update 0221-3.cpp

--- Basic Programming Problems/0221-3.cpp
+++ Basic Programming Problems/0221-3.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
 	int h;
 	cout<<"Please enter the triangle height=> ";
-    cin>>h;
-    cout<<endl;
+        cin>>h;
+        cout<<endl;
 	for(int i=1;i<=h;i++){
 		for(int j=1;j<=h-i;j++){
 			cout<<' ';
