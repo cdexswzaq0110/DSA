cc7e600820a793ca7e6f222641cda09bcbfb0dcd
TerryHC <112371712+cdexswzaq0110@users.noreply.github.com>
2025-02-25 15:05:31 +0800

    Update 0221-4.cpp

--- Basic Programming Problems/0221-4.cpp
+++ Basic Programming Problems/0221-4.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
 	int height;
 	cout<<"Please enter the triangle height=> ";
-    cin>>height;
-    cout<<endl;
+        cin>>height;
+        cout<<endl;
 	for(int i=1;i<=height;i++){
 		for(int j=1;j<=2*height-1;j++){
 			if(i+j==height+1||j-i==height-1||i==height) //左斜邊、右斜邊、底邊 
